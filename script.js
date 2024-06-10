3f70d67b2baae5a88b4b5d337e23667284338134
Arkaprava Gaine <codearka2002@gmail.com>
2024-06-10 21:06:17 +0530

    New project in scrimba course for js - counter app

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+let console_count = document.getElementById("count-el")
+let save_text = document.getElementById("save-text")
+
+
+
+let count = 0
+function increment(){
+    console.log("Arka1")
+    count += 1
+    console_count.textContent = count 
+}
+
+function save(){
+    console.log("Arka2")
+    save_text.textContent += " "+count+" / "
+    count = 0
+    console_count.textContent = 0 
+    
+}
+
+function cleartext(){
+    console.log("Arka3")
+    save_text.textContent = "Previous Entries : "
+    console_count.textContent = 0 
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+let name = "Arka"
+let greeting = "Welcome back to the website"
+
+
+let welcomeEl = document.getElementById("welcome-user")
+
+welcomeEl.innerText = name + " "+ greeting
+welcomeEl.innerText +=  " we are glad to have you back with us."
